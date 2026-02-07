d76c660104c2b3e60815da17c509196f0438dd04
Lora Johnson <108235672+Lora-Johnson@users.noreply.github.com>
2026-02-06 19:31:40 -0600

    Add note on unemployment data ingestion and updates
    Added a note about data ingestion and update frequency.

--- CREATE TABLE unemployment forecast.sql
+++ CREATE TABLE unemployment forecast.sql
@@ -3,3 +3,6 @@ CREATE TABLE unemployment_forecast (
     state VARCHAR(2) NOT NULL,
     unempl_ex DECIMAL(5,2) NOT NULL,
     INDEX idx_unemp_state (state));
+/* NOTE: The umempl_ex data can be ingested into a data warehouse via .csv download from https://oui.doleta.gov/unemploy/data_summary/SummaryTables.pdf. 
+Since the organization updates this data only quarterly, it doesn't make sense to create a pipeline. */
+
